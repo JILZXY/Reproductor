19f144498165736050adc7f6dfdf3a6d93f23139
JILZXY <Sasosiso79@gmail.com>
2025-10-26 08:49:39 -0600

    se quito el seguimiento a las variables de entorno jejejej

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  spotify: {
-    API_URL: 'https://api.spotify.com/v1',
-    AUTH_API_URL: 'https://accounts.spotify.com/api/token',
-    CLIENT_ID: 'e095f07367da45abb8687133d28a04ed',
-    CLIENT_SECRET: '23e6ba9feefe4181a5d812d71a8afa39'
-  }
-};
