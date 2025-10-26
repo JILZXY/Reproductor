3d2ced15b4a2c3c001033827cfca25ac6d4735fc
JILZXY <Sasosiso79@gmail.com>
2025-10-25 20:24:58 -0600

    generacion del componente search-bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Song } from './song/song';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Song],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
